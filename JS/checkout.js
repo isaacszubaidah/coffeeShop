537711e3e03d7d32a521a1f676a3db09a0de7946
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-12-06 20:25:23 +0200

    add

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,17 +1,45 @@
 let purchased = JSON.parse(localStorage.getItem("purchased"));
 let table = document.querySelector("main");
 
-table.innerHTML = purchased.map((item, index) => {
-  return `
-   <div class="productCard">
-   <img class="photo width-25 rounded-2 border p-3" src=${item.url}>
-   <h3 class="itemName">${item.name}</h3>
-   <p class="text">${item.description}</p>
-   <p>R${item.price}</p>
-</div>
+// Create a map to store item counts
+let itemCounter = new Map();
 
-    `
-}).join('');
+// Function to remove an item from the purchased array
+function removeFromCheckout(itemName) {
+  purchased = purchased.filter((item) => item.name !== itemName);
+  checkOutItems();
+  // Update the local storage with the modified purchased array
+  localStorage.setItem("purchased", JSON.stringify(purchased));
+}
+
+// Function to update the display after modifying the purchased array
+function checkOutItems() {
+  // Count the occurrences of each item
+  itemCounter.clear();
+  purchased.forEach((item) => {
+    const itemName = item.name;
+    itemCounter.set(itemName, (itemCounter.get(itemName) || 0) + 1);
+  });
+
+  // Generate HTML with item information and counts
+  table.innerHTML = [...itemCounter.keys()].map((itemName) => {
+    const item = purchased.find((purchasedItem) => purchasedItem.name === itemName);
+    const count = itemCounter.get(itemName);
+
+    return `
+      <div class="productCard">
+        <img class="photo width-25 rounded-2 border p-3" src=${item.url}>
+        <h3 class="itemName">${itemName}</h3>
+        <p class="text">${item.description}</p>
+        <p>R${item.price}</p>
+        <p>Count: ${count}</p>
+        <button onclick="removeFromCheckout('${itemName}')">Remove</button>
+      </div>
+    `;
+  }).join('');
+}
+checkOutItems();
+
 function getCurrentYear() {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
