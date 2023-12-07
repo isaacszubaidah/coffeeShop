1e0f73ad62c7f4b00e3d1111982105b8b12c7b10
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-12-07 22:16:57 +0200

    wip

--- JS/contact.js
+++ JS/contact.js
@@ -1,8 +1,8 @@
 function getCurrentYear() {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    document.getElementById("currentYear").innerText = currentYear;
-  }
-  
-  // Call the function to set the current year when the page loads
-  getCurrentYear();
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  document.getElementById("currentYear").innerText = currentYear;
+}
+
+// Call the function to set the current year when the page loads
+getCurrentYear();
